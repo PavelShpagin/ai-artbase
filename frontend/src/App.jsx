f6753e0a1b58687d81148a1bdb3944c69f40f815
PavelShpagin <pavelandrewshpagin@gmail.com>
2024-04-14 12:12:14 +0300

    first test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from "react";
+import { render } from "react-dom";
+import Gallery from "react-photo-gallery";
+import "./App.css";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const photos = [
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 63,
+    height: 32,
+    id: "unique-photo-id-1",
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1,
+    id: "unique-photo-id-2",
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4,
+    id: "unique-photo-id-3",
+  },
+];
 
-export default App
+const handleImageClick = (event, { photo, index }) => {
+  console.log("Clicked on photo ID:", photo.id);
+};
+
+const App = () => (
+  <Gallery photos={photos} direction={"column"} onClick={handleImageClick} />
+);
+
+export default App;
